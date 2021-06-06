b6c83d0282d763e6120a5d6c3f4228fddb951a11
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-06-06 19:37:27 +0200

    Design the UI for recently opened files

--- amrts-map/WelcomeScreen.xaml.cs
+++ amrts-map/WelcomeScreen.xaml.cs
@@ -19,9 +19,21 @@ namespace amrts_map
     /// </summary>
     public partial class WelcomeScreen : Window
     {
+        public List<string[]> RecentlyOpenedFiles = new List<string[]>();
         public WelcomeScreen()
         {
             InitializeComponent();
+            LoadRecentlyOpenedFiles();
+        }
+
+        private void LoadRecentlyOpenedFiles(bool loadExternally = false)
+        {
+            if (loadExternally) throw new NotImplementedException();
+
+            // Create samples
+            for (int i = 1; i <= 10; i++)
+                RecentlyOpenedFiles.Add(new string[] { String.Format("Sample Project {0}", i), Environment.ExpandEnvironmentVariables(String.Format(@"%userprofile%\Desktop\Project{0}\Project{0}.amramp", i)) });
+            foreach (string[] value in RecentlyOpenedFiles) lb_recent.Items.Add(String.Format("{0}\r\n{1}{2}", value[0], "          ", value[1]));
         }
 
         private void ContinueNoMap(object sender, RoutedEventArgs e)
@@ -30,6 +42,15 @@ namespace amrts_map
             this.Hide();
             mainWindow.ShowDialog();
             this.Show();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lb_recent.SelectedIndex == -1) return;
+            string selectedItem = (string)lb_recent.SelectedItem;
+            string[] selectedItemArray = selectedItem.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            int selectedIndex = lb_recent.SelectedIndex;
+            MessageBox.Show(String.Format("Selected ID: {0}\r\nItem Name: {1}\r\nItem Path: {2}", selectedIndex, selectedItemArray[0], selectedItemArray[1].Trim()));
         }
     }
 }
