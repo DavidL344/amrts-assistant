7aaaf5693dc14309828b3811e5470103f0eb1675
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-06-06 16:45:16 +0200

    Design the UI

---
+++ amrts-map/WelcomeScreen.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace amrts_map
+{
+    /// <summary>
+    /// Interaction logic for WelcomeScreen.xaml
+    /// </summary>
+    public partial class WelcomeScreen : Window
+    {
+        public WelcomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ContinueNoMap(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Hide();
+            mainWindow.ShowDialog();
+            this.Show();
+        }
+    }
+}
