65f493916bd72c5474c4ddb91f2077df79772d63
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-06-13 19:10:55 +0200

    Add argument validation checks

--- DrPack.Bridge/Run.cs
+++ DrPack.Bridge/Run.cs
@@ -11,6 +11,8 @@ namespace DrPack.Bridge
     {
         public static void Create(string file, string location, bool recursive = true, string mask = null)
         {
+            if (string.IsNullOrWhiteSpace(file)) throw new ArgumentException("The archive path is not defined!");
+            if (string.IsNullOrWhiteSpace(location)) throw new ArgumentException("The directory location is not defined!");
             string arguments = String.Format("c");
             if (recursive) arguments += " -r";
             if (mask != null) arguments += String.Format(" -m=*.{0}", mask);
@@ -21,6 +23,8 @@ namespace DrPack.Bridge
 
         public static void Extract(string file, string location)
         {
+            if (string.IsNullOrWhiteSpace(file)) throw new ArgumentException("The archive path is not defined!");
+            if (string.IsNullOrWhiteSpace(location)) throw new ArgumentException("The directory location is not defined!");
             Command.Execute(String.Format("x {0} {1}", file, location));
             if (!Directory.Exists(location)) throw new IOException("The directory couldn't be created!");
             if (!Directory.EnumerateFileSystemEntries(location).Any()) throw new IOException("The archive wasn't extracted!");
@@ -28,6 +32,7 @@ namespace DrPack.Bridge
 
         public static string List(string file)
         {
+            if (string.IsNullOrWhiteSpace(file)) throw new ArgumentException("The archive path is not defined!");
             return Command.Execute(String.Format("l {0}", file), true, true);
         }
     }
