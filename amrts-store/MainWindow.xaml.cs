e34b9b78bac4f5c419dbe6fb99c1f1477f54a2be
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-06-23 16:04:20 +0200

    Draft the basic store design

---
+++ amrts-store/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using amrts_store.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace amrts_store
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToPage(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                contentFrame.Navigate(typeof(Settings));
+            }
+            else
+            {
+                var selectedItem = (ModernWpf.Controls.NavigationViewItem)args.SelectedItem;
+                string pageName = "amrts_store.Pages." + (string)selectedItem.Tag;
+                Type pageType = typeof(Home).Assembly.GetType(pageName);
+                if (pageType != null) contentFrame.Navigate(pageType);
+            }
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            nv_main.SelectedItem = nv_main.MenuItems[0];
+            contentFrame.Navigate(typeof(Home));
+        }
+    }
+}
